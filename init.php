92b75fc309b5bc5359798c4e8c5bbc86e1d9c6d5
Reza Esmaili <dfox@dfox-imac.local>
2012-11-19 23:45:22 +0100

    Added composer specific switch

--- init.php
+++ init.php
@@ -1,4 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load Swiftmailer
-include Kohana::find_file('vendor', 'swiftmailer/lib/swift_required', 'php');
+if (!USES_COMPOSER) {
+    include Kohana::find_file('vendor', 'swiftmailer/lib/swift_required', 'php');
+}
