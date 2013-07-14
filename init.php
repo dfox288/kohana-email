2b7ff27bb8ec3e519164f9c893e1c2610443b9bd
rodrigo <rodrigo@corp.sonic.net>
2013-07-13 17:12:42 -0700

    checks for constant definition

--- init.php
+++ init.php
@@ -1,6 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load Swiftmailer
-if (!USES_COMPOSER) {
+if (
+	!defined('USES_COMPOSER')
+	|| defined('USES_COMPOSER') && !USES_COMPOSER
+) {
     include Kohana::find_file('vendor', 'swiftmailer/lib/swift_required', 'php');
 }
